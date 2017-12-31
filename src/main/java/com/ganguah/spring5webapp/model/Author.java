2643032f1ccb288b26cd671417bb987dc5ef8ca5
GeorgeAnguah <Georgeanguah@yahoo.com>
2017-12-31 02:08:08 -0500

    adding POJO

---
+++ src/main/java/com/ganguah/spring5webapp/model/Author.java
@@ -0,0 +1,59 @@
+package com.ganguah.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Author {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    Set<Book> books = new HashSet<>();
+
+
+    public Author(String firstName, String lastName, HashSet<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashSet<Book> books) {
+        this.books = books;
+    }
+
+
+}
